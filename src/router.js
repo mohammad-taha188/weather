25ecca1a6f01348e0eef65aa06d436fd50a1b684
zenitsu2088 <zenitsu2088@gmail.com>
2025-01-27 15:35:06 +0330

    create weather app

---
+++ src/router.js
@@ -0,0 +1,9 @@
+import cloud from "./assets/cloud.svg";
+import sun from "./assets/sun.svg";
+import rain from "./assets/rain.svg";
+import storm from "./assets/storm.svg";
+import snow from "./assets/snow.svg";
+import fog from "./assets/fog.svg";
+import tornado from "./assets/tornado.svg";
+
+export let images = { cloud, sun, rain, storm, snow, fog, tornado };
